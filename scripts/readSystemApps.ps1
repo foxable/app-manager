1a30cd61566a844e455c023cdc6bea5b7b749fbc
Sebastian Fuchs <foxable@web.de>
2017-04-02 17:35:33 +0200

    Generate full list of installed apps

--- scripts/readSystemApps.ps1
+++ scripts/readSystemApps.ps1
@@ -1,5 +1,11 @@
-Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
-    Select-Object @{name="name";expression={$_.DisplayName}},
-                  @{name="installedVersion";expression={$_.DisplayVersion}} |
-    Where-Object {$_.name -ne $null} |
-    ConvertTo-Json
+
+$l1 = Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*
+$l2 = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*
+$l3 = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
+
+$l1 + $l2 + $l3 |
+Select-Object @{name="name";expression={$_.DisplayName}},
+              @{name="installedVersion";expression={$_.DisplayVersion}} |
+Where-Object {$_.name -ne $null} |
+Sort-Object -Property name -Unique |
+ConvertTo-Json
